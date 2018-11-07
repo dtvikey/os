2dda5e23d518effb244b6b0e74a23e00b81ef957
dtvikey <dtvikey@qq.com>
2018-11-07 14:50:48 +0800

    v1.0

---
+++ src/main/java/com/imooc/os/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.imooc.os.service;
+
+import com.imooc.os.entity.Order;
+
+/**
+ * @Author: dtvikey
+ * @Date: 18-11-7 下午2:47
+ * @Version 1.0
+ */
+public interface OrderService {
+    void addOrder(Order order);
+}
